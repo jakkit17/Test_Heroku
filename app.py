5998425fbce7e47a1c4d30a2e28b69d303ef2b2d
jakkit17 <78692345+jakkit17@users.noreply.github.com>
2021-10-22 01:34:58 +0700

    jn

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, flash
+
+#initialize Flask( <app module> )
+app = Flask(__name__)
+app.secret_key = "super secret key"
+
+@app.route("/hello")
+def index():
+	flash("what's your name?")
+	return render_template("index.html")
+
+@app.route("/greet", methods=["POST", "GET"])
+def greet():
+	flash("Hi, " + str(request.form['name_input']) +". Nice to meet you.")
+	return render_template("index.html")
+
+
+# Fix error 500 - Set the secret_key
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
+# run on anaconda - install flask
+# c:\> conda activate env39
